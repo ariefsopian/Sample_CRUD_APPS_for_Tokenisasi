c2d633a9cde3e97d0c8b289f980170b429543576
ariefsopian <ariefsopiann@gmail.com>
2025-08-08 20:24:31 +0700

    perbaharui file .sql

---
+++ pegawai_202508082023.sql
@@ -0,0 +1,28 @@
+INSERT INTO db_pegawai.pegawai (nama_lengkap,tempat_lahir,tanggal_lahir,jenis_kelamin,agama,alamat,nomor_telepon,user_id) VALUES
+	 ('oOlH< O{?tqb','Bandung','2025-06-16','Laki-laki','Islam','y~qN)7%','C)i{.@/aQC)S',2),
+	 (']5,) |S7s&l}','Surabaya','1994-02-17','Laki-laki','Kristen','L@[F-jm','/m(Al<tocW',1),
+	 ('Wc,8XN? pZ{)fyDb','Bandung','1992-06-03','Laki-laki','Budha','_WT{dZ B%@',';~U_BYoX*_CS',1),
+	 ('R.ojW PYUMEpn','Bandung','1994-09-14','Perempuan','Islam','uo}pX5A a1QuCAu','Bv+~],u;%Af4(',1),
+	 ('!,K; h*dRN?U h&y&5','Bandung','2003-11-13','Laki-laki','Islam','!5!aN(Y aA|ke','WnIvxF+jlCWf',1),
+	 ('39{Ua i;v1_ u<8E/i','Bandung','2000-02-28','Perempuan','Islam','uo}pX5A a1QuCAu ','dA%Vv~:D94S6=',1),
+	 ('Y0]<_ v^-','Banyumas','1998-06-16','Laki-laki','Islam','lqv]?g!S /7e6R|x=','?(XDu*2>;)4<',1),
+	 ('c;T5$r QmXNL6QbJW','Depok','1999-05-16','Laki-laki','Islam','xXRd@t_','ylvsb1MC%x/Lrh',1),
+	 ('C2UsQ:2XK Qfcg(','Ambarawa','1991-12-12','Laki-laki','Islam','ol%4!?9n %f<n7n[l','u?U@ZC?|:?[O@',1),
+	 ('CUH%e]6p m]oU0(5B=','Surabaya','1991-04-12','Laki-laki','Islam','h<;>9j1U ','h1K7w!f/|i1',1);
+INSERT INTO db_pegawai.pegawai (nama_lengkap,tempat_lahir,tanggal_lahir,jenis_kelamin,agama,alamat,nomor_telepon,user_id) VALUES
+	 ('ldGO% xmSHW','Solo','1992-06-28','Laki-laki','Islam','Z!t!',']WE*X=K05&z#',1),
+	 ('LC{0r [g_$5J','Surabaya','2001-06-11','Laki-laki','Islam','h<;>9j1U','AQc72-IZwI!R7G',1),
+	 ('fKA;i u9tYCgX','Boyolali','1992-08-18','Laki-laki','Islam','r[Ui XMes7df','Ppm/sDMfV7Wh!',1),
+	 ('D|4,(L','Jakarta','2000-02-10','Laki-laki','Islam','nN2Z/ F:RX0p0 qRo;:','Q4.02?3D{OZ<',NULL),
+	 ('Rp~ o6b;!kf','Jakarta','1990-05-28','Laki-laki','Islam','9g9$[sPy] }zb','8M4P;KUU=]4',NULL),
+	 ('ldGO% xmSHW','Surabaya','1988-07-07','Laki-laki','Islam','MpWyp','c1D)f0>_ew<M%~{',NULL),
+	 ('G89/ 28bBVeB@','Padang','1994-07-08','Laki-laki','Islam','j1uh[>D ','jlC{z{b-oB4EF',NULL),
+	 ('{U5<4 dI1_Y(~','Bekasi','1994-02-13','Laki-laki','Islam','NrFg,3a ##.&w nMVhrH','w}Y{T5{nC04Y',NULL),
+	 ('{ox7a -]~lZ9','Cibubur','1994-04-16','Laki-laki','Islam','G0,7:04=_','EQM[Vii;QnlX#9',NULL),
+	 ('~-!?t /iNf^4<nW','Banyumas','1994-06-14','Laki-laki','Islam','I1ECiO4o','Hq*JR?Y!wp1',NULL);
+INSERT INTO db_pegawai.pegawai (nama_lengkap,tempat_lahir,tanggal_lahir,jenis_kelamin,agama,alamat,nomor_telepon,user_id) VALUES
+	 ('oWd+3 yfw?','Kalimatan','1992-09-16','Laki-laki','Islam','*G*5Apbv2','<T2l[!<Q)^m',NULL),
+	 (',*4/j, 1}}Qyr','Bandung','1992-06-17','Laki-laki','Islam','r[Ui XMes7df','7;O5,uL?i{z',NULL),
+	 ('KKS}8}. Y;1vx','Bekasi','1991-06-14','Laki-laki','Islam','|kaS me.578T qHT<34_,','B!,DB76.v1e&s',NULL),
+	 ('{;JH?e.Ge?','Cianjur','1994-04-14','Laki-laki','Islam','Ke- eBk)GjP 3RnJ|l/go','PY+hN>vrP/7iyqE',NULL),
+	 ('tJ5U 97=gEIQ','Solo','1993-07-12','Laki-laki','Kristen','mPL< 37y#Z8P','iIQwj}irDs0|@Xs',NULL);
